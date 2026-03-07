68873785f8cae45b544f9b627907cff5447bb28e
Saharier Omi <gsaharier761@gmail.com>
2026-03-07 13:23:22 +0600

    Add navbar and search box

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const login = () => {
-    const userName = document.getElementById("user-name").value;
-    const password = document.getElementById("password").value;
-    if (userName === "admin" && password === "admin123") {
-        window.location.href = "./home.html";
-    } else {
-        alert("Invalid username or password");
-    }
-}
+  const userName = document.getElementById("user-name").value;
+  const password = document.getElementById("password").value;
+  if (userName === "admin" && password === "admin123") {
+    window.location.href = "./home.html";
+  } else {
+    alert("Invalid username or password");
+  }
+};
+
